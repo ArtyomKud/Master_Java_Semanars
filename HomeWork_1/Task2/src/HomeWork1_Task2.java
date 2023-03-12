651bf36be05779052c1bbf30110c15f7a42f0ce2
Artyom <artyom_kudryavcev_91@mail.ru>
2023-03-12 22:47:41 +0700

    added solution task2 in HomWork1

--- HomeWork_1/Task2/src/HomeWork1_Task2.java
+++ HomeWork_1/Task2/src/HomeWork1_Task2.java
@@ -1,6 +1,29 @@
+
+//Вывести все простые числа от 1 до 1000
 public class HomeWork1_Task2 {
     public static void main(String[] args) {
+        PrimeNumber();
 
+    }
+    static void PrimeNumber ()
+    {
+        System.out.print(2 + " ");
 
+        for(int i = 3; i<1001; i++)
+        {
+            Boolean flag = true;
+            for(int j = 2; j<i; j++)
+            {
+                if (i%j==0)
+                {
+                    flag = false;
+                }
+            }
+            if (flag)
+            {
+                System.out.print(i+" ");
+            }
+
+        }
     }
 }
