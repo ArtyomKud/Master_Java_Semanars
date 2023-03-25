39735a20bcfa035ab6215c7f505e44000e21af10
Artyom <artyom_kudryavcev_91@mail.ru>
2023-03-25 15:27:37 +0700

    added the code to project HomeWork2_Task1

--- HomeWork2/HomeWork2_Task1/src/Main.java
+++ HomeWork2/HomeWork2_Task1/src/Main.java
@@ -1,2 +1,25 @@
 public class Main {
+    public static void main(String[] args) {
+        String str = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+        str = str.replaceAll(":", " ");
+        String[] str2 = str.split(" ");
+        for(int i = 0; i < str2.length; i++)
+        {
+            str2[i] = str2[i].replaceAll("[ ,:{}\" ]","");
+        }
+
+        StringBuilder str3 = new StringBuilder();
+        str3.append( "select * from students where ");
+        for(int i = 1; i < str2.length; i+=2){
+            if(str2[i].equals("null")|str2[i].equals(" ")){
+                continue;
+            }
+            str3.append(str2[i-1]);
+            str3.append(" = '");
+            str3.append(str2[i]);
+            str3.append("' ");
+        }
+        System.out.println(str3);
+    }
+
 }
