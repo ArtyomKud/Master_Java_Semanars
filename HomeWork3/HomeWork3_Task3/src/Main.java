5172cbec2493fa0e246c4b60b01c3e7a8cf1662c
Artyom <artyom_kudryavcev_91@mail.ru>
2023-03-26 16:37:49 +0700

    added code for working with arraylist

--- HomeWork3/HomeWork3_Task3/src/Main.java
+++ HomeWork3/HomeWork3_Task3/src/Main.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
+import java.util.stream.Stream;
+
 public class Main {
+    public static void main(String[] args) {
+        int size = 10;
+        int range = 100;
+        Random random = new Random();
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i <size; i ++) {
+            list.add((Integer)random.nextInt(range));
+        }
+        for (Integer item:list) {
+            System.out.print(item+" ");
+
+        }
+        System.out.println();
+
+
+
+        System.out.println(Collections.min(list));
+        System.out.println(Collections.max(list));
+        System.out.print(list.stream().mapToInt(a -> a).average().orElse(0));
+    }
 }
