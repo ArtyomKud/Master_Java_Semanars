38225f61d92b2aa09ee171be8417c5ec6ac3367e
Artyom <artyom_kudryavcev_91@mail.ru>
2023-03-26 19:24:22 +0700

    added code in HomeWork4_Task2

--- HomeWork4/HomeWork4_Task2/src/Main.java
+++ HomeWork4/HomeWork4_Task2/src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.LinkedList;
 
+public class Main {
+    /*Реализуйте очередь с помощью LinkedList со следующими методами:
+            • enqueue() — помещает элемент в конец очереди,
+            • dequeue() — возвращает первый элемент из очереди и удаляет его,
+• first() — возвращает первый элемент из очереди, не удаляя.*/
+    public static void main(String[] args) {
+        LinkedList <Integer> list = new LinkedList<>();
+        enqueue(1,list);
+        int a = dequeue(list);
+        int b = first(list);
+
+
+
+    }
+
+    private static int first(LinkedList<Integer> list) {
+        return list.getFirst();
+    }
+
+    private static int dequeue(LinkedList<Integer> list) {
+        return list.removeFirst();
+    }
+
+    private static void enqueue(int i, LinkedList<Integer> list) {
+        list.addLast(i);
     }
 }
